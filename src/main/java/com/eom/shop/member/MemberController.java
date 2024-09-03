387af1446ebe34e8eb0677fcc22c7cc996dfc77c
엄태성 <6380714@gmail.com>
2024-09-04 00:45:35 +0900

    dto 추가

--- src/main/java/com/eom/shop/member/MemberController.java
+++ src/main/java/com/eom/shop/member/MemberController.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -46,5 +46,21 @@ public class MemberController {
         var result = (CustomUser) auth.getPrincipal();
         System.out.println(auth.getPrincipal());
         return "mypage.html";
+    }
+    @GetMapping("/user/1")
+    @ResponseBody
+    public MemberDto getUser(){
+        var a = memberRepository.findById(1L);
+        var result = a.get();
+        var data = new MemberDto(result.getUsername(), result.getDisplayName());
+        return data;
+    }
+}
+class MemberDto{
+    public String username;
+    public String displayName;
+    MemberDto(String a, String b){
+        this.username = a;
+        this.displayName = b;
     }
 }
