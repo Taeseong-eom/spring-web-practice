ed3160a910dfa607b4d59ab67e5d125ab766b1a4
엄태성 <6380714@gmail.com>
2024-09-14 15:57:50 +0900

    JPQL 적용

--- src/main/java/com/eom/shop/sales/SalesController.java
+++ src/main/java/com/eom/shop/sales/SalesController.java
@@ -1,6 +1,7 @@
 package com.eom.shop.sales;
 
 import com.eom.shop.member.CustomUser;
+import com.eom.shop.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -25,16 +26,17 @@ public class SalesController {
 		sales.setPrice(price);
 		sales.setItemName(title);
 		CustomUser user = (CustomUser) auth.getPrincipal();
-//		System.out.println(user.id);
-		salesRepository.save(sales);
+		var member = new Member();
+		member.setId(user.id);
+		sales.setMember(member);
 
 		return "list.html";
 	}
 
 	@GetMapping("/order/all")
 	String getOrderAll(){
-		List<Sales> result = salesRepository.findAll();
-		System.out.println(result.get(0));
+		List<Sales> result = salesRepository.customerFindAll();
+		System.out.println(result);
 
 		return "list.html";
 	}
